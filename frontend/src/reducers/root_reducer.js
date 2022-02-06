ca311c5bd289335215cf95d74320c9fd1aa59bf5
HelenEdwards <hgedwards87@gmail.com>
2022-02-06 13:52:39 -0800

    trying to fix frontend issues

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
-import errors from './errors_reducer';
+import SessionErrorsReducer from './session_errors_reducer';
 
 const RootReducer = combineReducers({
   session,
-  errors
+  SessionErrorsReducer
 });
 
 export default RootReducer;
